6c685e38a430c8e4b21a6d862469f9179db10212
Lucas <lucassantana.github@gmail.com>
2026-05-07 17:29:46 +0000

    regulando arquivos

--- script/telaProfessor.js
+++ script/telaProfessor.js
@@ -1,9 +1,8 @@
- function toggleCard(card) {
+function toggleCard(card) {
    const isActive = card.classList.contains('active');
    document.querySelectorAll('.module-card').forEach(c => c.classList.remove('active'));
    if (!isActive) card.classList.add('active');
-<<<<<<< HEAD
+
  } 
-=======
- }  
->>>>>>> 504ec5506d7a012ccc8f074a66ace9254bb37751
+
+ 
