31eeb54404edb22522f2ac2e93752bc9aef1fb05
Lucas <lucassantana.github@gmail.com>
2026-05-09 22:31:12 +0000

    finalizando conexao

--- script/app.js
+++ script/app.js
@@ -1,32 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAFzy56P9sgF6DIXKfaXvwIR1dlnhKKiU8",
-  authDomain: "bancobiblioteca-24029.firebaseapp.com",
-  databaseURL: "https://bancobiblioteca-24029-default-rtdb.firebaseio.com",
-  projectId: "bancobiblioteca-24029",
-  storageBucket: "bancobiblioteca-24029.firebasestorage.app",
-  messagingSenderId: "691451184435",
-  appId: "1:691451184435:web:c5b7492af74bcf6a116a46"
-};
+src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js"
+src="https://www.gstatic.com/firebasejs/10.12.0/firebase-auth-compat.js"
 
-const src= src("bancobiblioteca-24029.firebaseapp-compat.com")
-const auth= src("bancobiblioteca-24029.firebaseauth-compat.com")
-const app = initializeApp(firebaseConfig);
 
-firebaseConfig.initializeApp(firebaseConfig)
+  const firebaseConfig = {
+    apiKey: "AIzaSyAFzy56P9sgF6DIXKfaXvwIR1dlnhKKiU8",
+    authDomain: "bancobiblioteca-24029.firebaseapp.com",
+    databaseURL: "https://bancobiblioteca-24029-default-rtdb.firebaseio.com",
+    projectId: "bancobiblioteca-24029",
+    storageBucket: "bancobiblioteca-24029.firebasestorage.app",
+    messagingSenderId: "691451184435",
+    appId: "1:691451184435:web:c5b7492af74bcf6a116a46"
+  };
 
-console.log("antes")
-firebase.auth().singInWithEmaillAndPassword("lucas@123", "123").then(Response=>{
-  console.log("sucesso", Response)
+  // Inicializa apenas uma vez
+  firebase.initializeApp(firebaseConfig);
 
-}).catch(error=>{
-  console.log("erro", error)
+  console.log("antes");
 
-})
-console.log("depois")
+  firebase.auth()
+    .signInWithEmailAndPassword("lucassantanadias2008@gmail.com", "220423")
+    .then(response => {
+      console.log("sucesso", response);
+    })
+    .catch(error => {
+      console.log("erro", error);
+    });
 
+  console.log("depois");
