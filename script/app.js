e04db6f20525b2d3f0dafacd3dc089aa5d883201
Lucas <lucassantana.github@gmail.com>
2026-05-09 20:14:48 +0000

    criando o arquivo de conexao

--- script/app.js
+++ script/app.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFzy56P9sgF6DIXKfaXvwIR1dlnhKKiU8",
+  authDomain: "bancobiblioteca-24029.firebaseapp.com",
+  databaseURL: "https://bancobiblioteca-24029-default-rtdb.firebaseio.com",
+  projectId: "bancobiblioteca-24029",
+  storageBucket: "bancobiblioteca-24029.firebasestorage.app",
+  messagingSenderId: "691451184435",
+  appId: "1:691451184435:web:c5b7492af74bcf6a116a46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
