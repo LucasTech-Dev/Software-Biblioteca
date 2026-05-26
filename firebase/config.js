48fa2a666bc2f190cd7fb64d483097dfc9338d07
Lucas Sant'ana <lucassantana.github@gmail.com>
2026-05-26 01:46:58 -0300

    terminando de criar banco no chrome

--- firebase/config.js
+++ firebase/config.js
@@ -1,22 +1,18 @@
-import { initializeApp }
+import { initializeApp }  
+//from "firebase/app";
 
 from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
 
+
 // CONFIG
 const firebaseConfig = {
-
-  apiKey: "AIzaSyAFzy56P9sgF6DIXKfaXvwIR1dlnhKKiU8",
-
-  authDomain: "bancobiblioteca-24029.firebaseapp.com",
-
-  projectId: "bancobiblioteca-24029",
-
-  storageBucket: "bancobiblioteca-24029.firebasestorage.app",
-
-  messagingSenderId: "691451184435",
-
-  appId: "1:691451184435:web:c5b7492af74bcf6a116a46"
+  apiKey: "AIzaSyCUisepO_NJCm2mZHWQczgVB5AplDJa0LA",
+  authDomain: "biblioteca-riachuelo.firebaseapp.com",
+  projectId: "biblioteca-riachuelo",
+  storageBucket: "biblioteca-riachuelo.firebasestorage.app",
+  messagingSenderId: "375143207365",
+  appId: "1:375143207365:web:89548892164c039785bbac"
 };
 
 
